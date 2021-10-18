da6e6a2d8ae32b3dc9a52f1dd6ddba434cba0b2f
ScParis <schpariss@gmail.com>
2021-10-18 18:37:36 -0300

    Submiting Project

---
+++ src/ao/co/a2x/library/model/Author.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ao.co.a2x.library.model;
+
+/**
+ *
+ * @author scherman
+ */
+public class Author {
+    
+    private String name;
+    private String age;
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName (String name){
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
